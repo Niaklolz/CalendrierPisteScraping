5d1628bf2319afae06546a5eaa10f544029ce22b
mlb <mathieu@lebot.org>
2020-11-25 20:23:32 +0100

    init project

---
+++ CalendrierPisteScraping/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalendrierPisteScraping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
